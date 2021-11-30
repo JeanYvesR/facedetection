332a58ee3e7aa11025dd1a7865ce02db59d1df46
JeanYvesR <JYRdev@outlook.com>
2021-11-30 10:27:19 -0300

    Updates

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component{
 	};
 	
 	onSubmitSignIn = () => {
-		fetch("http://localhost:3001/signin", {
+		fetch("https://serene-mesa-90870.herokuapp.com/signin", {
 			method: "post",
 			headers: {"Content-Type": "application/json"},
 			"body": JSON.stringify({
